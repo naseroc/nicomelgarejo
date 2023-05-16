3da0c4a297363cbc09395432fd177de4032ba380
Nicolas Melgarejo Sabelle <nicolas.melgarejos@gmail.com>
2023-05-16 14:11:31 -0400

    fixit partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 
 
-// https://astro.build/config
-export default defineConfig({
-    integrations: [
-      partytown({
-        // Adds dataLayer.push as a forwarding-event.
-        config: {
-          forward: ["dataLayer.push"],
-        },
-      }),
-    ],
-  });
+export default defineConfig ({
+  integrations: [partytown({
+    // Example: Add dataLayer.push as a forwarding-event.
+    config: { 
+      forward: ["dataLayer.push"] 
+    },
+  })],
+})
